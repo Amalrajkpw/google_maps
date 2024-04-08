888139678828510f63173af28626ee2b467cce95
Amalrajkpw <amalrajkp24@gmail.com>
2024-04-08 14:42:21 +0530

    inital_commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+ import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MapsApp());
+ }
+ class MapsApp extends StatelessWidget {
+   const MapsApp({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return const  MaterialApp(
+       home:MapScreen() ,
+     );
+   }
+ }
+ class MapScreen extends StatelessWidget {
+   const MapScreen({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return const Placeholder();
+   }
+ }
+ 
+ 

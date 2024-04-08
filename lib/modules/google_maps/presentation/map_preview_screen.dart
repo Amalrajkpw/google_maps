0cf651fbb26578663f7002b387eb53cf6f176757
Amalrajkpw <amalrajkp24@gmail.com>
2024-04-08 16:53:38 +0530

    first_commit

---
+++ lib/modules/google_maps/presentation/map_preview_screen.dart
@@ -0,0 +1,9 @@
+ import 'package:flutter/material.dart';
+class MapPreviewScreen extends StatelessWidget {
+  const MapPreviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
